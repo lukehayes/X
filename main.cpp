55ce94a53af0a686b09ace2f8775ffa98007ed8c
lukehayes <lukepurplehayes@gmail.com>
2024-09-25 09:44:22 +0100

    Added latest test code to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
-#include "XX/System/Engine.h"
+#include "raylib.h"
 
-int main(int argc, char *argv[]) {
+#include "X/Global.h"
+#include "X/GameObject.h"
 
-    constexpr int SCREEN_SCALE = 10;
-    XX::System::Engine engine(320 * SCREEN_SCALE, 180 * SCREEN_SCALE, "Game");
-    engine.run();
+int main(void)
+{
+    X::Global global = X::Global::create_global();
+
+    InitWindow(global.screen_width, global.screen_height, global.title);
+    SetTargetFPS(global.FPS);
+
+    X::GameObject object1({100,100});
+    X::GameObject object2({400,500});
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(LIGHTGRAY);
+            DrawRectangle(
+                object1.transform.position.x,
+                object1.transform.position.y,
+                object1.transform.scale.x,
+                object1.transform.scale.y,
+                object1.color
+            );
+
+            DrawRectangle(
+                object2.transform.position.x,
+                object2.transform.position.y,
+                object2.transform.scale.x,
+                object2.transform.scale.y,
+                object2.color
+            );
+        EndDrawing();
+    }
+
+    CloseWindow();        // Close window and OpenGL context
+
+    return 0;
 }
