58ce968c4278c59db953fbea94465b5fd9e31192
lukehayes <lukepurplehayes@gmail.com>
2024-09-26 17:55:15 +0100

    Sprite Position is now const&

--- include/X/Sprite.h
+++ include/X/Sprite.h
@@ -8,7 +8,7 @@ namespace X
 {
 struct Sprite : public X::GameObject
 {
-    Sprite(const char* image, const Vector2 position);
+    Sprite(const char* image, const Vector2& position);
     ~Sprite();
     Texture2D texture;
 };
--- src/X/Sprite.cpp
+++ src/X/Sprite.cpp
@@ -3,13 +3,12 @@
 
 namespace X
 {
-Sprite::Sprite(const char* image, const Vector2 position)
+Sprite::Sprite(const char* image, const Vector2& position)
         : GameObject(position),
           texture(LoadTexture(image)) {}
 
 Sprite::~Sprite()
 {
-    printf("Unloaded Sprite \n");
     UnloadTexture(this->texture);
 }
 }
