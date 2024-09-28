4816f06b1ab461ac6796621b26d1a600e3ef7597
lukehayes <lukepurplehayes@gmail.com>
2024-09-28 08:25:37 +0100

    Added Sprite copy assign, move assign and move ctors

--- include/X/Sprite.h
+++ include/X/Sprite.h
@@ -8,8 +8,18 @@ namespace X
 {
 struct Sprite : public X::GameObject
 {
+    Sprite() {}
     Sprite(const char* image, const Vector2& position);
+    Sprite(const Sprite& other);
+
+    Sprite(Sprite&& other);
+    Sprite& operator=(Sprite& other);
+    Sprite& operator=(Sprite&& other);
+
+
     ~Sprite();
+
+    const char* path;
     Texture2D texture;
 };
 }
--- src/X/Sprite.cpp
+++ src/X/Sprite.cpp
@@ -3,14 +3,43 @@
 
 namespace X
 {
-Sprite::Sprite(const char* image, const Vector2& position)
-        : GameObject(position),
-          texture(LoadTexture(image)) {}
+    Sprite::Sprite(const char* image, const Vector2& position)
+          : GameObject(position, WHITE),
+            path(image),
+            texture(LoadTexture(image)) {}
 
-Sprite::~Sprite()
-{
-    UnloadTexture(this->texture);
-}
+    Sprite::Sprite(const Sprite& other)
+          : GameObject(other.transform.position, WHITE),
+            path(other.path),
+            texture(LoadTexture(other.path)) {}
+
+    Sprite::Sprite(Sprite&& other)
+          : GameObject(other.transform.position, WHITE),
+            path(other.path),
+            texture(LoadTexture(other.path)) {}
+
+
+    // Sprite::Sprite& operator=(Sprite& other)
+    // {
+    //     if(this == &other)
+    //     {
+    //         return *this;
+    //     }
+    //
+    //     printf("-----------> Sprite Copied \n");
+    //     return *this;
+    // }
+    //
+    // Sprite::Sprite& operator=(Sprite&& other)
+    // {
+    //     printf("-----------> Sprite Move Assigned \n");
+    //     return *this;
+    // }
+
+    Sprite::~Sprite()
+    {
+        UnloadTexture(this->texture);
+    }
 }
 
 
