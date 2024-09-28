ef1f14c2caa608ff1eaa325fb5d89b4104b32a6c
lukehayes <lukepurplehayes@gmail.com>
2024-09-28 08:39:22 +0100

    Main now using unique_ptr

--- main.cpp
+++ main.cpp
@@ -45,17 +45,18 @@ int main(void)
     X::GameObject object1({100,100});
     X::Sprite s1("../assets/debug.png", Vector2({200,200}));
 
-    // std::vector<GOUptr>objects;
-    std::vector<X::GameObject*> objects;
+    std::vector<GOUptr>objects;
 
     for(int i = 0; i<= 10; i++)
     {
         float rx = GetRandomValue(0, global.screen_width - 20);
         float ry = GetRandomValue(0, global.screen_height - 20);
 
-        X::Sprite* o = new X::Sprite{"../assets/debug.png", {rx,ry} };
+        std::unique_ptr<X::Sprite> o = std::make_unique<X::Sprite>("../assets/debug.png", Vector2 {rx,ry});
 
-        objects.push_back(o);
+        // UPtr<X::Sprite> o = std::make_unique<X::Sprite>("../assets/debug.png", {rx,ry});
+        //
+        objects.push_back(std::move(o));
 
         // std::unique_ptr<X::GameObject> o;
         // o = std::make_unique<X::Sprite>("../assets/debug.png", Vector2({rx,ry}));
@@ -70,10 +71,8 @@ int main(void)
 
             for(auto& obj : objects)
             {
-                DrawObject(*obj);
-                
-                auto spr = dynamic_cast<X::Sprite*>(obj);
-            
+                X::Sprite* spr = dynamic_cast<X::Sprite*>(obj.get());
+
                 DrawSprite(spr);
 
                 // auto spr = dynamic_cast<X::Sprite*>(&obj);
@@ -87,11 +86,6 @@ int main(void)
             // }
 
         EndDrawing();
-    }
-
-    for(auto& obj : objects)
-    {
-        delete obj;
     }
 
     std::cout << "Deleted All objects" << std::endl;
