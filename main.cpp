3a1221d45d261e1753ee1fb254e5227ab97cc102
lukehayes <lukepurplehayes@gmail.com>
2024-10-01 10:03:29 +0100

    Updated fixed timestep loop in main

--- main.cpp
+++ main.cpp
@@ -49,37 +49,44 @@ int main(void)
     spr1->game = &game;
     objects.push_back(std::move(spr1));
 
-    std::chrono::milliseconds start = std::chrono::duration()::zero;
-    std::chrono::milliseconds end;
-    double tick = 0;
-    double ns = 1000000.0 / 60.0; // Syncs updates at 60 per second (59 - 61)
+    using secs = std::chrono::milliseconds;
+    using millis = std::chrono::microseconds;
+    using sys_clock = std::chrono::high_resolution_clock;
+
+    millis startMS  = std::chrono::duration_cast<millis>(sys_clock::now().time_since_epoch());
+    double start = 0.0, end = 0.0;
+    double tick  = 0.0;
+    double ns    = 1000000.0 / 60.0; // Syncs updates at 60 per second (59 - 61)
 
     while (!WindowShouldClose()) {
 
-        auto start = (std::chrono::steady_clock::now()).time_since_epoch();
+        // auto start = (std::chrono::steady_clock::now()).time_since_epoch();
 
-        std::chrono::milliseconds m = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+        // Get milli seconds
+        startMS = std::chrono::duration_cast<millis>(sys_clock::now().time_since_epoch());
+        start   = startMS.count();
 
-        std::cout << m.count() << std::endl;
+        tick  += (double)(start - end) / ns;
+        end   = start;
 
+        std::cout << "Tick: " << tick << std::setprecision(10) << std::endl;
+        std::cout << "start: " << start << std::setprecision(10) << std::endl;
+        std::cout << "end: " << end << std::setprecision(10) << std::endl;
+        std::cout << "Start - end: " << (start - end) << std::endl;
+        std::cout << "Start - end: / ns " << (start - end) / ns << std::endl;
+        std::cout << "End: " << end << std::endl;
 
-        start = (std::chrono::steady_clock().now()).time_since_epoch();
-
-        // tick  += (double)(start - end) / ns;
-
-        std::cout << "Tick" << std::endl;
-
-        while (tick >= 1.0) {
-            
-            // Call tick() here
-
-            std::cout << "Tick" << std::endl;
-
-            tick -=1.0;
-        }
+        // while (tick >= 1.0) {
+        //
+        //     // Call tick() here
+        //
+        //     std::cout << "Tick Update: " << tick << std::setprecision(10) << std::endl;
+        //     tick -= 1.0;
+        // }
 
 
         std::cout << "Update" << std::endl;
+
         for(auto& obj : objects)
         {
             obj->update();
