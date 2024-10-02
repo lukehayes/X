8da9dd595383dee7c3e42b56487e3a47efa89657
lukehayes <lukepurplehayes@gmail.com>
2024-10-02 09:56:45 +0100

    Started implemented better fixed timestep

--- main.cpp
+++ main.cpp
@@ -60,57 +60,65 @@ int main(void)
 
     millis startMS  = std::chrono::duration_cast<millis>(sys_clock::now().time_since_epoch());
     
-    auto t1 = std::chrono::steady_clock().now();
-    time_point t2;
-
-    long double start = 0.0, end = 0.0;
+    auto previous = std::chrono::steady_clock().now();
+    // auto current = std::chrono::steady_clock::now();
+    long double elapsed = 0.0;
     long double tick  = 0.0;
-    long double ns    = 1000000.0 / 60.0; // Syncs updates at 60 per second (59 - 61)
+    long double MS_PER_UPDATE = 0.033;
 
     while (!WindowShouldClose()) {
 
-        t1 = std::chrono::steady_clock().now();
-        start = t1.time_since_epoch().count();
+        auto current = std::chrono::steady_clock().now();
+        long double elapsed = current.time_since_epoch().count() - previous.time_since_epoch().count();
+        previous = current;
 
-        std::cout << "Start " << start << std::endl;
-        std::cout << "End " << end << std::endl;
-        std::cout << "start - end " << (start - end) << std::endl;
-        std::cout << "start - end / ns " << (long double)(start - end) / ns << std::endl;
-        std::cout << "ns" << ns << std::endl;
-        std::cout << "   " << std::endl;
-        std::cout << "---------" << std::endl;
+        auto t = std::chrono::duration<double, std::ratio<1,10000>>(current.time_since_epoch().count());
+        // tick = t.count();
+        tick += elapsed;
+        // auto tick2 = std::chrono::duration<long double>(tick).count();
 
-        // auto n = std::chrono::duration_cast<std::chrono::duration<double>>(t1 - t2);
+        std::cout << "Elapsed " << elapsed << std::endl;
+        std::cout << "Tick " << tick << std::endl;
+        std::cout << "t " << t << std::endl;
 
-        // tick  += (double)n.count() / ns;
-        tick  += (double)(start - end) / ns;
-        end = start;
-
-        std::cout << "tick " << tick << std::endl;
-
-        game.tick = tick;
-        while (tick >= 1.0) {
-
-            // Call n.count()() here
-            for(auto& obj : objects)
-            {
-                X::Sprite* spr = static_cast<X::Sprite*>(obj.get());
-
-                spr->update();
-            }
-
-            std::cout << "---------> tick Update: " << (long double)tick << std::endl;
-            tick -= 1.0;
-        }
+        // std::chrono::duration<double,std::ratio<1,1000>>
+        // auto n = std::chrono::duration_cast<std::chrono::duration<double>>(previous - current);
+        
+        /**--------------------------------------------------------------------
+        INPUT
+        --------------------------------------------------------------------**/
 
 
+
+        /**--------------------------------------------------------------------
+        TICK PHYSICS
+        --------------------------------------------------------------------**/
+        // while(tick >= MS_PER_UPDATE)
+        // {
+        //     for(auto& obj : objects)
+        //     {
+        //         obj->tick();
+        //     }
+        //
+        //     std::cout << "UPDATING. Currrent Tick: " << tick << std::endl;
+        //     tick -= MS_PER_UPDATE;
+        //     game.tick = tick;
+        // }
+
+
+        /**--------------------------------------------------------------------
+        UPDATE DELTA
+        --------------------------------------------------------------------**/
         std::cout << "Update" << std::endl;
 
         for(auto& obj : objects)
         {
-            obj->update();
+            // obj->update();
         }
 
+        /**--------------------------------------------------------------------
+        RENDER
+        --------------------------------------------------------------------**/
         BeginDrawing();
             ClearBackground(LIGHTGRAY);
             std::cout << "Render" << std::endl;
@@ -119,7 +127,8 @@ int main(void)
             {
                 X::Sprite* spr = static_cast<X::Sprite*>(obj.get());
 
-                spr->render();
+                obj->render();
+                // spr->render();
             }
 
         EndDrawing();
