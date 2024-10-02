d260fe15a2de9bb67762303c77ada176f2c123d8
lukehayes <lukepurplehayes@gmail.com>
2024-10-02 09:56:59 +0100

    Added more fixed loop test example

--- fixed.cpp
+++ fixed.cpp
@@ -1,3 +1,29 @@
+// Munificent version
+
+double previous = getCurrentTime();
+double lag = 0.0;
+
+while (true)
+{
+  double current = getCurrentTime();
+  double elapsed = current - previous;
+  previous = current;
+  lag += elapsed;
+
+  processInput();
+
+  while (lag >= MS_PER_UPDATE)
+  {
+    update();
+    lag -= MS_PER_UPDATE;
+  }
+
+  render();
+}
+
+
+
+
 long int start = 0, end = 0;
 double delta = 0;
 double ns = 1000000.0 / 60.0; // Syncs updates at 60 per second (59 - 61)
