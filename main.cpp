d2e7d4a0c519c6a5702af89cd73f07992cc88636
lukehayes <lukepurplehayes@gmail.com>
2025-03-27 09:41:28 +0000

    feat: add simple SDL3 window creation and destruction

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <iostream>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     // ------------------------------------------------------------------------
     // Set initial state here.
 
-    return 0;
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+    SDL_Window* window = SDL_CreateWindow("Title", 800,600, SDL_WINDOW_OPENGL);
+
+    if (!window) {
+
+        std::cout << "Window Initialization Failed" << std::endl;
+        std::cout << "Error:" << SDL_GetError() << std::endl;
+    }
+
+    SDL_DestroyWindow(window);
 }
