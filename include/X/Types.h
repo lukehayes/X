7d93311345de2c59122f7a2fe3cb87842778c9bf
lukehayes <lukepurplehayes@gmail.com>
2025-03-28 08:45:19 +0000

    feat: add Types.h to project

---
+++ include/X/Types.h
@@ -0,0 +1,15 @@
+#ifndef X_TYPES_H
+#define X_TYPES_H
+
+using i16   = int;
+using i32   = long int;
+using i64   = long long int;
+
+using ui16  = unsigned int;
+using ui32  = unsigned long int;
+using ui64  = unsigned long long int;
+
+using f32   = float;
+using cStr  = const char*;
+
+#endif // !X_TYPES_H
