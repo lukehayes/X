e900e53d92072ae3454dff925c372619601345bb
lukehayes <lukepurplehayes@gmail.com>
2025-05-17 11:12:37 +0100

    feat: add X/GL/Shader.h

---
+++ include/X/GL/Shader.h
@@ -0,0 +1,28 @@
+
+#include <string>
+
+namespace X::GL
+{
+	class Shader
+	{
+	public:
+		Shader(const char* vsh, const char* fsh);
+
+		Shader(
+			const std::string& vsh,
+			const std::string& fsh
+		);
+
+		Shader(const Shader& rhs);
+		Shader(const Shader&& rhs);
+
+		Shader& operator=(Shader& rhs);
+		Shader& operator=(Shader&& rhs);
+
+		~Shader();
+
+	private:
+		std::string vsh;
+		std::string fsh;
+	}
+}
