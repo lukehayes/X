551b478e018b5c0e02d3dabb02e772394e5dd3a6
lukehayes <lukepurplehayes@gmail.com>
2025-05-30 10:15:21 +0100

    fix: opengl context is now created correctly

--- main.cpp
+++ main.cpp
@@ -1,18 +1,37 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-#include <GL/gl.h>
 #include <iostream>
+#include "glad/glad.h"
+
+#include "X/GL/Shader.h"
 
 int main(int argc, char *argv[])
 {
     // ------------------------------------------------------------------------
     // Set initial state here.
 
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
-
     bool isRunning           = true;
+
+    if(!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initialize SDL: %s\n", SDL_GetError());
+        std::cout << "Window Context Failed" << std::endl;
+        std::cout << "Error:" << SDL_GetError() << std::endl;
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+
     SDL_Window* window       = SDL_CreateWindow("Title", 800,600, SDL_WINDOW_OPENGL);
     SDL_GLContext window_ctx = SDL_GL_CreateContext(window);
+
+    SDL_GL_MakeCurrent(window, window_ctx);
+
+    // INITIALIZE GLAD:
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        throw(std::string("Failed to initialize GLAD"));
+    }
 
     if (window == NULL) {
 
@@ -22,10 +41,16 @@ int main(int argc, char *argv[])
     }
 
     if (!window_ctx) {
-
         std::cout << "Window Context Failed" << std::endl;
         std::cout << "Error:" << SDL_GetError() << std::endl;
     }
+
+
+    //X::GL::Shader default_shader(
+        //"VSH-Default.glsl",
+        //"FSH-Default.glsl"
+    //);
+
 
     while (isRunning) {
         SDL_Event event;
@@ -37,7 +62,8 @@ int main(int argc, char *argv[])
         }
 
         // Test basic OPENGL works
-        glClearColor(0,0,1,1);
+        float cv = 0.25;
+        glClearColor(cv,cv,cv,1);
         glClear(GL_COLOR_BUFFER_BIT);
 
         SDL_GL_SwapWindow(window);
