de5f85d7d6a0ab94fbb1e30a9df199b553086cab
lukehayes <lukepurplehayes@gmail.com>
2025-06-05 09:49:38 +0100

    feat: add X::GL::BufferType

---
+++ include/X/GL/BufferType.h
@@ -0,0 +1,13 @@
+#include "glad/glad.h"
+
+/**
+* Wrapper for OpenGL buffer types
+*/
+namespace X
+{
+	enum BufferType
+	{
+		ARRAY_BUFFER = GL_ARRAY_BUFFER,
+		ELEMENT_BUFFER = GL_ELEMENT_ARRAY_BUFFER
+	};
+}
