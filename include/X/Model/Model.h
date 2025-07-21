896bd36bc803e65f65cafc89867da604a0818ea0
lukehayes <lukepurplehayes@gmail.com>
2025-07-21 10:08:31 +0100

    feat: add rotation, scale and rotation angle to Model

--- include/X/Model/Model.h
+++ include/X/Model/Model.h
@@ -12,7 +12,10 @@ public:
 	~Model();
 
 	glm::vec3 position = {0,0,-0.1f};
+	glm::vec3 rotation = {0,0,1};
+	glm::vec3 scale = {1,1,1};
 	glm::mat4 matrix = glm::mat4(1.0f);
+	float rotAngle = 0.0f;
 };
 }
 
