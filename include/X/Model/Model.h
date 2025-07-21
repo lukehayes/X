8d59ba1d7111c39b25ab55fd3adc66486e05a09d
lukehayes <lukepurplehayes@gmail.com>
2025-07-21 09:44:55 +0100

    feat: add Model::Model class

---
+++ include/X/Model/Model.h
@@ -0,0 +1,19 @@
+#ifndef MODEL_MODEL_H
+#define MODEL_MODEL_H
+
+#include "X/Math/GLM.h"
+
+namespace Model
+{
+class Model
+{
+public:
+	Model() {}
+	~Model() {}
+
+	glm::vec3 position = {0,0,-0.1f};
+    glm::mat4 matrix = glm::mat4(1.0f);
+};
+}
+
+#endif // MODEL_MODEL_H
