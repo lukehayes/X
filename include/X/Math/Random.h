44fe9dc6327d3c94b9e976e74544f458b5db7ad1
lukehayes <lukepurplehayes@gmail.com>
2025-08-23 11:42:12 +0100

    style: X::Random functions now pascal case.

--- include/X/Math/Random.h
+++ include/X/Math/Random.h
@@ -10,13 +10,13 @@ class Random
 {
 public:
 
-    static int randInt(int from, int to)
+    static int RandInt(int from, int to)
     {
         std::random_device device;
         std::uniform_int_distribution<int> dist(from, to);
         return dist(device);
     }
-    static double randDouble(double from, double to)
+    static double RandDouble(double from, double to)
     {
         std::random_device device;
         std::uniform_real_distribution<double> dist(from, to);
