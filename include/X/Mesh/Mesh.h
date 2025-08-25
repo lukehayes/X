586c8c5f7a06e87b3bd2d96ecbbb9032e9118ade
lukehayes <lukepurplehayes@gmail.com>
2025-08-25 16:11:25 +0100

    refactor: Mesh class now using X::GL::BufferData class

--- include/X/Mesh/Mesh.h
+++ include/X/Mesh/Mesh.h
@@ -1,9 +1,11 @@
 #ifndef X_MESH_MESH_H
 #define X_MESH_MESH_H
 
+#include "X/GL/BufferData.h"
 #include "X/GL/VertexArray.h"
 #include "X/GL/VertexBuffer.h"
 #include "X/GL/IndexBuffer.h"
+#include "glad/glad.h"
 
 namespace X::Mesh
 {
@@ -12,11 +14,20 @@ class Mesh
 {
 public:
 
-	Mesh() {}
+	Mesh(
+		X::GL::BufferData buffer_data,
+		X::GL::VertexArray vertex_array,
+		GLenum buffer_type
+	) : 
+		vao(std::move(vertex_array)),
+		buffer_data(std::move(buffer_data))
+	{}
+	Mesh(Mesh&&) = default;
 
-	X::GL::VertexArray vao;
+	X::GL::BufferData   buffer_data;
+	X::GL::VertexArray  vao;
 	X::GL::VertexBuffer vbo;
-	X::GL::IndexBuffer ibo;
+	X::GL::IndexBuffer  ibo;
 
 	size_t vertex_count;
 	size_t index_count;
