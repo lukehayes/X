9e15c7aa6212309b175b9d22f231cf6ed8708a44
lukehayes <lukepurplehayes@gmail.com>
2025-08-28 17:13:11 +0100

    feat: X::Mesh::Mesh now accepts VAO, VBO and IBO

--- include/X/Mesh/Mesh.h
+++ include/X/Mesh/Mesh.h
@@ -1,7 +1,6 @@
 #ifndef X_MESH_MESH_H
 #define X_MESH_MESH_H
 
-#include "X/GL/BufferData.h"
 #include "X/GL/VertexArray.h"
 #include "X/GL/VertexBuffer.h"
 #include "X/GL/IndexBuffer.h"
@@ -14,20 +13,15 @@ class Mesh
 {
 public:
 
-	Mesh(
-		X::GL::BufferData buffer_data,
-		X::GL::VertexArray vertex_array,
-		GLenum buffer_type
-	) : 
-		vao(std::move(vertex_array)),
-		buffer_data(std::move(buffer_data))
+	Mesh(X::GL::VertexArray& vertex_array, X::GL::VertexBuffer& vertex_buffer, X::GL::IndexBuffer& index_buffer)
+		: vertex_array(std::move(vertex_array)),
+		  vertex_buffer(std::move(vertex_buffer)),
+		  index_buffer(index_buffer)
 	{}
-	Mesh(Mesh&&) = default;
 
-	X::GL::BufferData   buffer_data;
-	X::GL::VertexArray  vao;
-	X::GL::VertexBuffer vbo;
-	X::GL::IndexBuffer  ibo;
+	X::GL::VertexArray vertex_array;
+	X::GL::VertexBuffer vertex_buffer;
+	X::GL::IndexBuffer index_buffer;
 
 	size_t vertex_count;
 	size_t index_count;
