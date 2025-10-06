fbf3e8b477e7327444f2a8fd70a725ee97022780
lukehayes <lukepurplehayes@gmail.com>
2025-10-06 12:31:37 +0100

    refactor: add Transform(posittion) constructor

--- include/X/Transform.h
+++ include/X/Transform.h
@@ -8,6 +8,7 @@ namespace X
 struct Transform
 {
 	Transform();
+	Transform(glm::vec3 position);
 	Transform(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale);
 
 	glm::vec3 position;
--- src/X/Transform.cpp
+++ src/X/Transform.cpp
@@ -8,6 +8,12 @@ Transform::Transform()
 	  scale({1,1,1})
 {}
 
+Transform::Transform(glm::vec3 position)
+	: position(position),
+	  rotation({0,0,0}),
+	  scale({1,1,1})
+{}
+
 Transform::Transform(glm::vec3 position, glm::vec3 rotation, glm::vec3 scale)
 	: position(position),
 	  rotation(rotation),
