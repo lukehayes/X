caf86a64cf37ae069afc8d251e64272265d94b0e
lukehayes <lukepurplehayes@gmail.com>
2025-10-11 11:06:18 +0100

    refactor: changed orthographic projection values

--- src/X/Camera/Camera2D.cpp
+++ src/X/Camera/Camera2D.cpp
@@ -1,13 +1,15 @@
 #include "X/Camera/Camera2D.h"
 
+
 namespace X::Camera
 {
 
 Camera2D::Camera2D(float width, float height)
         : X::Camera::Camera(
-         glm::ortho(0.0f, width, 0.0f, height, 0.1f, 100.0f),
+         glm::ortho(width/height, -width/height, -1.0f,1.0f,0.1f,10.0f),
          glm::mat4(1.0f)
-        ) {}
+        ) {
+}
 
 Camera2D::~Camera2D() {}
 
