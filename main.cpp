4f729634aaacce6d781ad4fa2bf964d9c6bf1929
lukehayes <lukepurplehayes@gmail.com>
2026-01-16 10:52:18 +0000

    refactor: moved basic gl code to main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include "X/App.h"
-#include "X/Gfx/Renderer.h"
+#include "glad/glad.h"
+
 #include "X/Global.h"
 #include "X/GL/Shader.h"
 #include "X/Math/GLM.h"
 
-#include "X/Model/Model.h"
-
 #include "X/GL/VertexArray.h"
 #include "X/GL/VertexBuffer.h"
 #include "X/GL/IndexBuffer.h"
-#include "glad/glad.h"
+#include "X/GL/IndexBuffer.h"
+#include "X/GL/VertexAttribute.h"
+
+#include <inplace_vector>
 
 extern X::Global global;
 
@@ -26,34 +28,78 @@ int main(int argc, char *argv[]) {
 	X::App app(WIN_WIDTH, WIN_HEIGHT);
 
 
-	std::vector<GLfloat> verticies = {0.5f,  0.5f,  0.0f, 0.5f,  -0.5f, 0.0f,
-		-0.5f, -0.5f, 0.0f, -0.5f, 0.5f,  0.0f};
-
-	X::GL::VertexArray vertex_array;
-	// vertex_array.Bind();
-
-	X::GL::VertexBuffer vertex_buffer{
-		X::GL::ATTRIB_VERTEX_POSITION, 
-		3, 
-		0,
-		verticies, 
-		GL_ARRAY_BUFFER
+	std::vector<GLfloat> verticies = {
+		0.5f,  0.5f,  0.0f,
+		0.5f,  -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+		-0.5f, 0.5f,  0.0f
 	};
 
-	// vertex_buffer.Bind();
+	X::GL::VertexArray vertex_array;
+	vertex_array.Bind();
+
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	int vertex_size        = 3;
+	int vertex_stride      = 0;
+	GLenum dataType        = GL_FLOAT;
+	GLboolean isNormalized = GL_FALSE;
+	GLvoid* pointer        = (void*)0;
+
+	glEnableVertexAttribArray(X::GL::ATTRIB_VERTEX_POSITION);
+
+	glVertexAttribPointer(
+		X::GL::ATTRIB_VERTEX_POSITION,
+		vertex_size,
+		dataType,
+		isNormalized,
+		vertex_stride,
+		pointer
+	);
+
 
 	std::vector<unsigned int> indices = {
 		0, 1, 3, // first triangle
 		1, 2, 3  // second triangle
 	};
 
-	X::GL::IndexBuffer index_buffer{indices};
+	GLuint ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices.at(0)) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+
+
+
+	// X::GL::VertexBuffer vertex_buffer{
+	// 	X::GL::ATTRIB_VERTEX_POSITION, 
+	// 	3, 
+	// 	0,
+	// 	verticies, 
+	// 	GL_ARRAY_BUFFER
+	// };
+
+	// vertex_buffer.Bind();
+
+	// std::vector<unsigned int> indices = {
+	// 	0, 1, 3, // first triangle
+	// 	1, 2, 3  // second triangle
+	// };
+
+	// X::GL::IndexBuffer index_buffer{indices};
 
 
 	X::GL::Shader default_shader(
 		"../assets/shaders/VSH-Default.glsl",
 		"../assets/shaders/FSH-Default.glsl"
 	);
+
+
+	X::GL::VertexAttribute vertexPosition(0, verticies, 3, 0, 0);
+	X::GL::VertexAttribute vertexColor(1, verticies, 3, 0, 0);
 
 	// X::Model::Model model;
 	// model.color = {0.3, 0.3, 0.3, 1};
@@ -113,13 +159,14 @@ int main(int argc, char *argv[]) {
 
 		static float c = 0.0;
 
-		c += 1.0;
+		c += 0.1;
 
 		glClearColor(cv,cv,cv,1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		default_shader.use();
 		default_shader.setUniformVec4({0.2,0.2,0.2,0.2}, "color");
+		default_shader.setUniformFloat(c, "time");
 
 		// renderer.Draw(model, cam, default_shader);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
