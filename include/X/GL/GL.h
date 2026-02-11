2d1a886a2a80293ad6c61f84c5039d736718868b
lukehayes <lukepurplehayes@gmail.com>
2026-02-11 09:28:28 +0000

    feat: moved VertextAttribute functionality into GL.h

---
+++ include/X/GL/GL.h
@@ -0,0 +1,26 @@
+#ifndef X_GL_GL_H
+#define X_GL_GL_H
+
+#include "glad/glad.h"
+
+namespace X::GL
+{
+	struct GL
+	{
+		GLuint vao;
+		GLuint vbo;
+		GLuint ibo;
+	};
+
+void SetVertexAttribute(int position, int size, int stride, int pointer )
+{
+	glVertexAttribPointer(position,size,GL_FLOAT, GL_FALSE, stride * sizeof(float), (void*)(sizeof(float) * pointer));
+	glEnableVertexAttribArray(position);
+}
+
+}
+
+
+
+
+#endif  // X_GL_GL_H
