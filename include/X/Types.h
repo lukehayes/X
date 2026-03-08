d6aef68c6ca9f71d0f07dd606a69d60fa7e84c2f
lukehayes <lukepurplehayes@gmail.com>
2026-03-08 17:27:23 +0000

    feat: add glm types

--- include/X/Types.h
+++ include/X/Types.h
@@ -1,6 +1,9 @@
 #ifndef X_TYPES_H
 #define X_TYPES_H
 
+#include "X/Math/GLM.h"
+
+
 using i16   = int;
 using i32   = long int;
 using i64   = long long int;
@@ -11,5 +14,9 @@ using ui64  = unsigned long long int;
 
 using f32   = float;
 using cStr  = const char*;
+
+using Vec2 = glm::vec2;
+using Vec3 = glm::vec3;
+using Mat4 = glm::mat4;
 
 #endif // !X_TYPES_H
