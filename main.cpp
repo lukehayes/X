1dc6babcf6d47e426bf1c1ea2c7be87f292af7ae
lukehayes <lukepurplehayes@gmail.com>
2026-03-08 10:28:29 +0000

    refactor: add latest test code to main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "X/App.h"
-#include "X/GL/Shader.h"
 #include "X/Gfx/Renderer.h"
-#include "X/Math/GLM.h"
+#include "X/Mesh/Mesh.h"
 
 constexpr int WIN_MULT   = 4;
 constexpr int WIN_WIDTH  = 320 * WIN_MULT;
@@ -15,14 +14,14 @@ int main(int argc, char *argv[]) {
 
 	X::App app(WIN_WIDTH, WIN_HEIGHT);
 
-	X::GL::Shader default_shader{"../assets/shaders/VSH-Default.glsl",
-		"../assets/shaders/FSH-Default.glsl"};
 
 	X::Gfx::Renderer renderer;
 
+	X::Mesh::Mesh planeMesh = X::Mesh::LoadQuadMesh();
+	X::Mesh::Mesh cubeMesh = X::Mesh::LoadCubeMesh();
+
 	// END OPENGL ----------------------------------------------------
 
-	float c = 0.0;
 
 	float deltaTime = 0;
 	Uint64 NOW = SDL_GetPerformanceCounter();
@@ -47,9 +46,21 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		c += 0.1;
+		renderer.Clear(0.2, 0.2, 0.2);
+		// renderer.WireFrame();
 
-		renderer.Clear(0.7, 0.7, 0.7);
+		// renderer.DrawMesh3D(
+		// 	cubeMesh,
+		// 	transform,
+		// 	{0.65,0.65,0.65}
+		// );
+
+		// renderer.DrawCube3DRaw(
+		// 	{0,0,0},
+		// 	{1,1,1},
+		// 	camera,
+		// 	default_shader
+		// );
 
 		SDL_GL_SwapWindow(app.GetWindow());
 	}
