46cb53178812724e4462f3f44d99d5371276e5d2
lukehayes <lukepurplehayes@gmail.com>
2026-03-16 10:42:24 +0000

    feat: add seperate Cube and Plane model generators

--- include/X/Model/Generator.h
+++ include/X/Model/Generator.h
@@ -2,19 +2,18 @@
 #define X_MODEL_GENERATOR_H
 
 #include <vector>
-#include "X/Model/Model.h"
+#include "X/Model/CubeModel.h"
+#include "X/Model/PlaneModel.h"
 #include "X/Math/Random.h"
 
 namespace X::Model
 {
-	std::vector<X::Model::Model> GenerateModels(int count, int scatter)
+	std::vector<X::Model::Model> GenerateCubeModels(int count, int scatter)
 	{
 		std::vector<X::Model::Model> models;
 
 		for(int i = 0; i <= count; i++)
 		{
-			X::Model::Model model;
-
 			double rx = X::Math::Random::RandDouble(-scatter, scatter);
 			double ry = X::Math::Random::RandDouble(-scatter, scatter);
 			double rz = X::Math::Random::RandDouble(-scatter, scatter);
@@ -27,6 +26,8 @@ namespace X::Model
 			double rg = X::Math::Random::RandDouble(0,1);
 			double rb = X::Math::Random::RandDouble(0,1);
 			double ra = X::Math::Random::RandDouble(1,1);
+
+			X::Model::CubeModel model { { rr,rg,rb,ra } };
 
 			model.transform.position.x = rx;
 			model.transform.position.y = ry;
@@ -41,7 +42,45 @@ namespace X::Model
 			model.color.b = rb;
 			model.color.a = ra;
 
+			models.push_back(model);
+		}
 
+		return models;
+	}
+	std::vector<X::Model::Model> GeneratePlaneModels(int count, int scatter)
+	{
+		std::vector<X::Model::Model> models;
+
+		for(int i = 0; i <= count; i++)
+		{
+
+			double rx = X::Math::Random::RandDouble(-scatter, scatter);
+			double ry = X::Math::Random::RandDouble(-scatter, scatter);
+			double rz = X::Math::Random::RandDouble(-scatter, scatter);
+
+			double rot_x = X::Math::Random::RandDouble(0, 360);
+			double rot_y = X::Math::Random::RandDouble(0, 360);
+			double rot_z = X::Math::Random::RandDouble(0, 360);
+
+			double rr = X::Math::Random::RandDouble(0,1);
+			double rg = X::Math::Random::RandDouble(0,1);
+			double rb = X::Math::Random::RandDouble(0,1);
+			double ra = X::Math::Random::RandDouble(1,1);
+
+			X::Model::PlaneModel model { { rr,rg,rb,ra } };
+
+			model.transform.position.x = rx;
+			model.transform.position.y = ry;
+			model.transform.position.z = rz;
+
+			model.transform.rotation.x = rot_x;
+			model.transform.rotation.y = rot_y;
+			model.transform.rotation.z = rot_z;
+
+			model.color.r = rr;
+			model.color.g = rg;
+			model.color.b = rb;
+			model.color.a = ra;
 
 			models.push_back(model);
 		}
